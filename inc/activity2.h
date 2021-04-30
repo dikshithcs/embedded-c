a2dfdf00de73a554af573367b0b55b7d4e9f4346
dikshithcs <dikshithcs123@gmail.com>
2021-04-30 11:58:49 +0530

    Create activity2.h

---
+++ inc/activity2.h
@@ -0,0 +1,17 @@
+/**
+ * @file activity2.h
+ * @author 256268 (dikshithcs123@gmail.com)
+ * @brief ADC function 
+ * @version 0.1
+ * @date 2021-04-29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef __READADC_H__
+#define __READADC_H__
+
+uint16_t ReadADC(uint8_t ch);//read adc funtion
+void InitADC();//initializing adc
+#endif
